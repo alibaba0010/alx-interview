6a18dff734041790a53af887a0289474d3c4f421
alibaba0010 <zakariyahali100@gmail.com>
2024-06-10 16:45:41 +0100

    rotate matrix task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,17 +3,22 @@
 """
 Given an n x n 2D matrix, rotate it 90 degrees clockwise
 """
+
+
 def rotate_2d_matrix(matrix):
-   # intialize roatated matrix
-   rotated_matrix = []
-   copied_row = 0
-   N = len(matrix)
-   for column in range(N):
-      for row in range((N-1), -1, -1):
-         if column == 0:
-            rotated_matrix.append([])
-            rotated_matrix[copied_row].append(matrix[row][column])
-      copied_row += 1
-      print(rotated_matrix)
-        
-   
+    # intialize roatated matrix
+    rotated_matrix = []
+    copied_row = 0
+    N = len(matrix)
+    for column in range(N):
+        for row in range((N - 1), -1, -1):
+            if column == 0:
+                rotated_matrix.append([])
+                rotated_matrix[copied_row].append(matrix[row][column])
+        copied_row += 1
+
+        # copy contents of matrix_copy into matrix
+        # since matrix is NxN, do not need to resize matrix for rotation
+    for row in range(N):
+        for column in range(N):
+            matrix[row][column] = rotated_matrix[row][column]
